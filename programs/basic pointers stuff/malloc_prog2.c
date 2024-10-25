b5c4936ef559c86b0b53cb541a3f5e7ebca41bfe
Rishab T <155544616+Schizoid-man@users.noreply.github.com>
2024-10-25 12:50:11 +0530

    Create malloc_prog2.c

---
+++ programs/basic pointers stuff/malloc_prog2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int *ptr;
+	int n = 5;
+	ptr = (int*)malloc(n*sizeof(int));
+	if (ptr ==NULL)
+	{
+		printf("Memory not allocated \n");
+		return 1;
+	}
+	printf("Memory successfully allocated using malloc \n");
+	for (int i = 0; i<n;i++)
+	{
+		ptr[i] = i+1;
+		printf("%d" , ptr[i]);
+	}
+	free(ptr);
+	return 0;
+}
