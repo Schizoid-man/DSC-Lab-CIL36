0142a6a2ad567e98409f3d2d53b43aa1d0a6a45e
Rishab T <155544616+Schizoid-man@users.noreply.github.com>
2024-11-15 12:44:58 +0530

    Create infix_to_postfix.c

---
+++ programs/infix_to_postfix.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+char stack[MAX];
+int top = -1;
+
+void push(char ch) {
+    if (top >= MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = ch;
+}
+
+char pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return '\0';
+    }
+    return stack[top--];
+}
+
+int isEmpty() {
+    return top == -1;
+}
+
+int isBalanced(char *expr) {
+    for (int i = 0; i < strlen(expr); i++) {
+        char ch = expr[i];
+        
+       
+        if (ch == '(' || ch == '{' || ch == '[') {
+            push(ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (isEmpty()) {
+                return 0;
+            }
+            char topChar = pop(); 
+           
+            if ((ch == ')' && topChar != '(') ||
+                (ch == '}' && topChar != '{') ||
+                (ch == ']' && topChar != '[')) {
+                return 0;
+            }
+        }
+    }
+    return isEmpty(); 
+}
+
+int main() {
+    char expr[MAX];
+    printf("Enter an expression: ");
+    
+    fgets(expr, sizeof(expr), stdin);
+    
+    expr[strcspn(expr, "\n")] = 0;
+
+    if (isBalanced(expr)) {
+        printf("The expression is balanced.\n");
+    } else {
+        printf("The expression is not balanced.\n");
+    }
+
+    return 0;
+}
